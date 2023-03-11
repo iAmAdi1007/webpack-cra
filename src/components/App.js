6bb92058859cdb9b9258119fdcd5899e11ba2a36
Aditya Pratap <pratapaditya516@gmail.com>
2023-03-12 01:19:10 +0530

    React Environment Setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+        <div>Create React App is Outdated!!</div>
+        <h1>Live Reload Enabled!</h1>
+    </>
+    
+  )
+}
+
+export default App
